05b43f2ea2b1df3554d80fed38cf6f093afd9757
antonio-farinhas <61586484+antonio-farinhas@users.noreply.github.com>
2020-10-15 14:02:32 +0100

    Update net.py

--- core/model/net.py
+++ core/model/net.py
@@ -1,9 +1,3 @@
-# --------------------------------------------------------
-# mcan-vqa (Deep Modular Co-Attention Networks)
-# Licensed under The MIT License [see LICENSE for details]
-# Written by Yuhao Cui https://github.com/cuiyuhao1996
-# --------------------------------------------------------
-
 from core.model.net_utils import FC, MLP, LayerNorm
 from core.model.mca import MCA_ED
 
